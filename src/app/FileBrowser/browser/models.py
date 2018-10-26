495bdfc1a5fb56440e4f9725cf9e6b912ddfc69b
josephblake8424 <43009785+josephblake8424@users.noreply.github.com>
2018-10-25 20:29:54 -0400

    Add files via upload

--- src/app/FileBrowser/browser/models.py
+++ src/app/FileBrowser/browser/models.py
@@ -26,7 +26,7 @@ class User(models.Model):
 
 class File(models.Model):
     FileID = models.CharField(primary_key=True, max_length = 30)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
     filePath = models.CharField(max_length=100)
     dataDate = models.DateField()
     SystemID = models.ForeignKey(System, on_delete = models.CASCADE)
