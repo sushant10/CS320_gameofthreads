20d87c07b97d6b91616a1e3db6d0729f88a9f111
sushant10 <sushdude34@gmail.com>
2018-11-13 00:35:47 -0500

    Added login authentication

--- src/app/FileBrowser/browser/models.py
+++ src/app/FileBrowser/browser/models.py
@@ -13,7 +13,7 @@ class System(models.Model):
         return self.serialNumberInserv
 
 class customUser(models.Model):
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=100)
     role = models.CharField(max_length=20)
 
--- src/app/FileBrowser/browser/views.py
+++ src/app/FileBrowser/browser/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.template import loader
-from .models import File, System
+from .models import *
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth.hashers import make_password, check_password
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import *
 
 # Create your views here.
 """
@@ -27,6 +30,9 @@ class SystemView(generic.ListView):
         return System.objects.all().order_by('-serialNumberInserv')[:100]
 
 def systems(request):
+    if not request.session.has_key('username'):
+        return redirect("browser:login")
+    username = request.session['username']
     system_list = System.objects.all().order_by('serialNumberInserv')[:100]
     counts = []
     for system in system_list:
@@ -34,7 +40,10 @@ def systems(request):
     system_count_list = zip(system_list, counts)
     return render(request, 'browser/systems_page.html', {'system_list' : system_count_list})
 
+
 def files(request, serialNumberInserv):
+    if not request.session.has_key('username'):
+        return redirect("browser:login")
     files = get_list_or_404(File, SystemID = serialNumberInserv)
     system = get_object_or_404(System, serialNumberInserv=serialNumberInserv)
     return render(request, 'browser/files_page.html', {'file_list':files, 'companyID':serialNumberInserv, 
@@ -42,11 +51,34 @@ def files(request, serialNumberInserv):
 
 
 def help(request):
+    if not request.session.has_key('username'):
+        return redirect("browser:login")
     return render(request, 'browser/help.html', {})
 
 def loginView(request):
-    return render(request, 'browser/login.html', {})
+    errors = []
+    if request.method == 'POST':
+        login_form = LoginForm(data=request.POST)
+        if login_form.is_valid():
+            usr = login_form.cleaned_data['username']
+            pswd = login_form.cleaned_data['password']
+            try:
+                u = customUser.objects.get(username=usr)
+                if check_password(pswd, u.password):
+                    request.session['username'] = usr
+                    return redirect("browser:systems")
+                else:
+                    errors = ['invalid password']
+            except ObjectDoesNotExist:
+                errors = ['invalid username']
+        else:
+            errors = ['invalid username or password']
+
+    return render(request, 'browser/login.html', {'error' : errors})
 
 def logoutView(request):
-    logout(request)
+    try:
+      del request.session['username']
+    except:
+      pass
     return redirect("browser:login")
