31de909e85cc89fd94bcf575cfa8ba25167d56eb
Danny <36552981+danielmaryanski@users.noreply.github.com>
2018-12-04 14:51:17 -0500

    Added gradient redness on Capacity column
    subject to change

--- src/app/FileBrowser/browser/static/js/systems_page.js
+++ src/app/FileBrowser/browser/static/js/systems_page.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
 			heightMatch: 'none'
 		}
 	});
+	
+	$(".capacity").each(function(){
+		var capacity_string = $(this).html();
+		var capacity = capacity_string.substr(0, capacity_string.length - 1)/100.0;		
+		capacity /= 0.3
+		var color = "rgba(" + (1-capacity/100.0)*255 + ","
+							+ capacity*255 + "," 
+							+ capacity*255 + ","
+							+ ((1-capacity)*0.4 + 0.2) + ")";
+		$(this).css("background-color", color);
+	})
+	
 });
 
 $(window).resize(function(){
