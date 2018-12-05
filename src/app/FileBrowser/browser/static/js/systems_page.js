f094deb1b73638611fa8a02fa8427db5a5ca0ba6
kzcote <43009815+kzcote@users.noreply.github.com>
2018-12-04 19:38:18 -0500

    Add files via upload
    Made a slight adjustment to the capacity coloring of specific cells to make the difference between numbers slightly above and below 30%

--- src/app/FileBrowser/browser/static/js/systems_page.js
+++ src/app/FileBrowser/browser/static/js/systems_page.js
@@ -12,11 +12,14 @@ $(document).ready(function(){
 	$(".capacity").each(function(){
 		var capacity_string = $(this).html();
 		var capacity = capacity_string.substr(0, capacity_string.length - 1)/100.0;		
-		capacity /= 0.3
-		var color = "rgba(" + (1-capacity/100.0)*255 + ","
-							+ capacity*255 + "," 
-							+ capacity*255 + ","
-							+ ((1-capacity)*0.4 + 0.2) + ")";
+		capacity /= 0.3 
+		if (capacity > 1){
+			return;
+		}
+		var color = "rgba(" + 220 + ","
+							+ 0 + "," 
+							+ 0 + ","
+							+ ((1-capacity)*0.5 + 0.2) + ")";
 		$(this).css("background-color", color);
 	})
 	
